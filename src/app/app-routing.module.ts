d1db73f8311dfbf280b7d09bc95a954010a42744
Joe Scharnitzke <j.scharnitzke@gmail.com>
2018-05-03 19:24:09 -0700

    Refactored server to init using a resolver

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -10,6 +10,7 @@ import { UsersComponent } from './users/users.component';
 import { AuthGuard } from './auth-guard.service';
 import { CanDeactivateGuard } from './servers/edit-server/can-deactivate-guard.service';
 import { ErrorPageComponent } from './error-page/error-page.component';
+import { ServerResolver } from './servers/server/server-resolver.service';
 
 const appRoutes: Routes = [
   { path: '', component: HomeComponent },
@@ -23,7 +24,11 @@ const appRoutes: Routes = [
     canActivateChild: [AuthGuard],
     component: ServersComponent,
     children: [
-      { path: ':serverId', component: ServerComponent },
+      {
+        path: ':serverId',
+        component: ServerComponent,
+        resolve: { server: ServerResolver }
+      },
       {
         path: ':serverId/edit',
         component: EditServerComponent,
--- src/app/servers/server/server.component.ts
+++ src/app/servers/server/server.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 import { ServersService } from '../servers.service';
-import { ActivatedRoute, Router } from '@angular/router';
+import { ActivatedRoute, Router, Data } from '@angular/router';
 
 @Component({
   selector: 'app-server',
@@ -9,22 +9,24 @@ import { ActivatedRoute, Router } from '@angular/router';
   styleUrls: ['./server.component.css']
 })
 export class ServerComponent implements OnInit {
-  server: {id: number, name: string, status: string};
+  server: { id: number; name: string; status: string };
 
-  constructor(private serversService: ServersService, private route: ActivatedRoute, private router: Router) { }
+  constructor(
+    private serversService: ServersService,
+    private route: ActivatedRoute,
+    private router: Router
+  ) {}
 
   ngOnInit() {
-    const serverId: number = this.route.snapshot.params.serverId ? this.route.snapshot.params.serverId : 1;
-    this.server = this.serversService.getServer(+serverId);
-
-    this.route.params.subscribe((serverParams) => {
-      const subServerId: number = serverParams.serverId ? serverParams.serverId : 1;
-      this.server = this.serversService.getServer(+subServerId);
+    this.route.data.subscribe((data: Data) => {
+      this.server = data.server;
     });
   }
 
   onEdit() {
-    this.router.navigate(['edit'], { relativeTo: this.route, queryParamsHandling: 'preserve' });
+    this.router.navigate(['edit'], {
+      relativeTo: this.route,
+      queryParamsHandling: 'preserve'
+    });
   }
-
 }
