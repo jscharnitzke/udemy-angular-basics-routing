d82f93ab4cb866b468873ccf0b3ced2df56b98b2
Joe Scharnitzke <j.scharnitzke@gmail.com>
2018-05-03 19:07:28 -0700

    Added deactivate route check when changes are pending in edit server

--- src/app/servers/edit-server/edit-server.component.ts
+++ src/app/servers/edit-server/edit-server.component.ts
@@ -1,37 +1,72 @@
 import { Component, OnInit } from '@angular/core';
 
 import { ServersService } from '../servers.service';
-import { ActivatedRoute, Router } from '@angular/router';
+import { ActivatedRoute, Router, Params } from '@angular/router';
+import { CanComponentDeactivate } from './can-deactivate-guard.service';
+import { Observable } from 'rxjs/Observable';
 
 @Component({
   selector: 'app-edit-server',
   templateUrl: './edit-server.component.html',
   styleUrls: ['./edit-server.component.css']
 })
-export class EditServerComponent implements OnInit {
-  server: {id: number, name: string, status: string};
+export class EditServerComponent implements OnInit, CanComponentDeactivate {
+  server: { id: number; name: string; status: string };
   serverName = '';
   serverStatus = '';
-  private allowEdit = false;
+  allowEdit = false;
+  changesSaved = false;
 
-  constructor(private serversService: ServersService, private route: ActivatedRoute, private router: Router) { }
+  constructor(
+    private serversService: ServersService,
+    private route: ActivatedRoute,
+    private router: Router
+  ) {}
 
   ngOnInit() {
     const serverId = this.route.snapshot.params.serverId;
     this.server = this.serversService.getServer(serverId ? +serverId : 1);
     this.serverName = this.server.name;
     this.serverStatus = this.server.status;
-    this.allowEdit = this.route.snapshot.queryParams.allowEdit;
+    this.allowEdit = this.route.snapshot.queryParams.allowEdit === 'true';
 
-    this.route.params.subscribe((serverParams) => {
-      this.server = this.serversService.getServer(serverParams.serverId ? +serverParams.serverId : 1);
+    this.route.params.subscribe(serverParams => {
+      this.server = this.serversService.getServer(
+        serverParams.serverId ? +serverParams.serverId : 1
+      );
       this.serverName = this.server.name;
       this.serverStatus = this.server.status;
+    });
+
+    this.route.queryParams.subscribe((queryParams: Params) => {
+      this.allowEdit = queryParams.allowEdit === 'true';
     });
   }
 
   onUpdateServer() {
-    this.serversService.updateServer(this.server.id, {name: this.serverName, status: this.serverStatus});
+    this.serversService.updateServer(this.server.id, {
+      name: this.serverName,
+      status: this.serverStatus
+    });
+
+    this.changesSaved = true;
+
+    this.router.navigate(['../'], { relativeTo: this.route });
   }
 
+  canDeactivate(): Observable<boolean> | Promise<boolean> | boolean {
+    if (!this.allowEdit) {
+      return true;
+    }
+
+    if (
+      (this.serverName !== this.server.name ||
+        this.serverStatus !== this.server.status) &&
+      !this.changesSaved
+    ) {
+      return confirm('Do you want to discard your changes?');
+    }
+
+    return true;
+  }
 }
