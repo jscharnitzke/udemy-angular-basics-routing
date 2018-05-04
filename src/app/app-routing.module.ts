1ecf1ffc9964e4ca500f616115874e7e8b3b80cb
Joe Scharnitzke <j.scharnitzke@gmail.com>
2018-05-03 19:13:37 -0700

    Added an error page that uses static passed data

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -9,6 +9,7 @@ import { UserComponent } from './users/user/user.component';
 import { UsersComponent } from './users/users.component';
 import { AuthGuard } from './auth-guard.service';
 import { CanDeactivateGuard } from './servers/edit-server/can-deactivate-guard.service';
+import { ErrorPageComponent } from './error-page/error-page.component';
 
 const appRoutes: Routes = [
   { path: '', component: HomeComponent },
@@ -30,7 +31,11 @@ const appRoutes: Routes = [
       }
     ]
   },
-  { path: 'not-found', component: PageNotFoundComponent },
+  {
+    path: 'not-found',
+    component: ErrorPageComponent,
+    data: { errorMessage: 'Page not found' }
+  },
   { path: '**', redirectTo: '/not-found' }
 ];
 
